6e505421b1383b89484bb4a2866cefecd5374915
teddy57350 <lallemandteddy@yahoo.fr>
2026-04-06 04:44:23 +0200

    Update checkout.js

--- pages/api/stripe/checkout.js
+++ pages/api/stripe/checkout.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
       mode: "subscription",
       line_items: [
         {
-          price: "price_1TGzLJLcn25N5LCHtJGUeLa4",
+        price: process.env.STRIPE_PRICE_ID,
           quantity: 1,
         },
       ],
